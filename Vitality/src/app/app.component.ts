c7fad16e247d49b060ee47e9d8b43cae5ddca9dd
Diegogh112 <u202217814@upc.edu.pe>
2024-06-11 17:04:12 -0500

    seguridad

--- Vitality/src/app/app.component.ts
+++ Vitality/src/app/app.component.ts
@@ -4,6 +4,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
+import { LoginService } from './services/login.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -15,10 +17,32 @@ import { MatButtonModule } from '@angular/material/button';
     MatMenuModule,
     MatButtonModule,
     RouterLink,
+    NgIf,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Vitality';
+  role: string = '';
+  constructor(private loginService: LoginService) {}
+
+  cerrar() {
+    sessionStorage.clear();
+  }
+
+  verificar() {
+    this.role = this.loginService.showRole();
+    return this.loginService.verificar();
+  }
+  isAdmin() {
+    return this.role === 'ADMIN';
+  }
+
+  isProfesional() {
+    return this.role === 'PROFESIONAL';
+  }
+  isCliente() {
+    return this.role === 'CLIENTE';
+  }
 }
--- Vitality/src/app/app.config.ts
+++ Vitality/src/app/app.config.ts
@@ -1,17 +1,28 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { JwtModule } from '@auth0/angular-jwt';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
+export function tokenGetter() {
+  return sessionStorage.getItem('token');
+}
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
     provideClientHydration(),
-    provideHttpClient(withFetch()), provideAnimationsAsync(), provideAnimationsAsync(),
+    provideHttpClient(withInterceptorsFromDi()),
+    importProvidersFrom(
+      JwtModule.forRoot({
+        config: {
+          tokenGetter: tokenGetter,
+          allowedDomains: ['localhost:8080'],
+          disallowedRoutes: ['http://localhost:8080/login/forget'],
+        },
+      })
+    ), provideAnimationsAsync(),
 
   ],
 };
