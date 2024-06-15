fdeedfab278c5ef4695e9ee89ebe74c695da7aa0
Diegogh112 <u202217814@upc.edu.pe>
2024-06-14 21:45:21 -0500

    a

--- Vitality/src/app/components/question/creaeditaquestion/creaeditaquestion.component.ts
+++ Vitality/src/app/components/question/creaeditaquestion/creaeditaquestion.component.ts
@@ -1,7 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Question } from '../../../models/question';
-import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
+import {
+  ActivatedRoute,
+  Params,
+  RouterLink,
+  RouterOutlet,
+} from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -11,7 +21,6 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
-
 
 @Component({
   selector: 'app-creaeditaquestion',
@@ -26,32 +35,104 @@ import { MatButtonModule } from '@angular/material/button';
     MatInputModule,
     MatFormFieldModule,
     ReactiveFormsModule,
-    ],
+  ],
   templateUrl: './creaeditaquestion.component.html',
-  styleUrl: './creaeditaquestion.component.css'
+  styleUrl: './creaeditaquestion.component.css',
 })
 export class CreaeditaquestionComponent implements OnInit {
-  form: FormGroup=new FormGroup({});
-  question:Question=new Question();
+  form: FormGroup = new FormGroup({});
+  question: Question = new Question();
+  id: number = 0;
+  edicion: boolean = false;
   listaPreguntas: { value: string; viewValue: string }[] = [
-    { value: '¿Qué vitaminas son esenciales para mejorar mi sistema inmunológico?', viewValue: '¿Qué vitaminas son esenciales para mejorar mi sistema inmunológico?' },
-    { value: '¿Qué suplementos recomiendan para aumentar la energía durante el día?', viewValue: '¿Qué suplementos recomiendan para aumentar la energía durante el día?' },
-    { value: '¿Cuál es la mejor proteína para ganar masa muscular rápidamente?', viewValue: '¿Cuál es la mejor proteína para ganar masa muscular rápidamente?' },
-    { value: '¿Qué recetas rápidas y fáciles me recomiendas para el desayuno?', viewValue: '¿Qué recetas rápidas y fáciles me recomiendas para el desayuno?' },
-    { value: '¿Qué suplemento de vitaminas recomiendan para mejorar la salud de la piel?', viewValue: '¿Qué suplemento de vitaminas recomiendan para mejorar la salud de la piel?' },
-    { value: '¿Tienen algún suplemento recomendado para mejorar la concentración y el enfoque?', viewValue: '¿Tienen algún suplemento recomendado para mejorar la concentración y el enfoque?' },
+    {
+      value:
+        '¿Qué vitaminas son esenciales para mejorar mi sistema inmunológico?',
+      viewValue:
+        '¿Qué vitaminas son esenciales para mejorar mi sistema inmunológico?',
+    },
+    {
+      value:
+        '¿Qué suplementos recomiendan para aumentar la energía durante el día?',
+      viewValue:
+        '¿Qué suplementos recomiendan para aumentar la energía durante el día?',
+    },
+    {
+      value: '¿Cuál es la mejor proteína para ganar masa muscular rápidamente?',
+      viewValue:
+        '¿Cuál es la mejor proteína para ganar masa muscular rápidamente?',
+    },
+    {
+      value: '¿Qué recetas rápidas y fáciles me recomiendas para el desayuno?',
+      viewValue:
+        '¿Qué recetas rápidas y fáciles me recomiendas para el desayuno?',
+    },
+    {
+      value:
+        '¿Qué suplemento de vitaminas recomiendan para mejorar la salud de la piel?',
+      viewValue:
+        '¿Qué suplemento de vitaminas recomiendan para mejorar la salud de la piel?',
+    },
+    {
+      value:
+        '¿Tienen algún suplemento recomendado para mejorar la concentración y el enfoque?',
+      viewValue:
+        '¿Tienen algún suplemento recomendado para mejorar la concentración y el enfoque?',
+    },
   ];
-  constructor (private formBuilder:FormBuilder, private qS:QuestionService, private router: Router, private route:ActivatedRoute){}
+  constructor(
+    private formBuilder: FormBuilder,
+    private qS: QuestionService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
   ngOnInit(): void {
-    this.form=this.formBuilder.group({
-      consulta:['',Validators.required]
-    })
+    this.route.params.subscribe((data: Params) => {
+      this.id = data['id'];
+      this.edicion = data['id'] != null;
+      this.init();
+    });
+
+    this.form = this.formBuilder.group({
+      codigo: [''],
+      consulta: ['', Validators.required],
+    });
   }
-  aceptar():void{
-    if (this.form.valid){this.question.consulta=this.form.value.consulta;
-      this.qS.insert(this.question).subscribe((data)=>{
-        this.qS.list().subscribe((data)=>{this.qS.setList(data);})});
+  aceptar(): void {
+    if (this.form.valid) {
+      this.question.idQuestion=this.form.value.codigo;
+      this.question.consulta = this.form.value.consulta;
+      if (this.edicion){
+        this.qS.update(this.question).subscribe((data)=>{
+          this.qS.list().subscribe((data)=>{
+            this.qS.setList(data);
+          })
+          this.router.navigate(['preguntas']);
+        })
+      }else{
+      this.qS.insert(this.question).subscribe((data) => {
+        this.qS.list().subscribe((data) => {
+          this.qS.setList(data);
+        });
         this.router.navigate(['preguntas']);
+      });
+    }
+
+    }
+  }
+
+  cancelar(): void {
+    this.router.navigate(['preguntas']);
+  }
+
+  init() {
+    if (this.edicion) {
+      this.qS.listid(this.id).subscribe((data) => {
+        this.form = new FormGroup({
+          codigo: new FormControl(data.idQuestion),
+          consulta: new FormControl(data.consulta),
+        });
+      });
     }
   }
 }
--- Vitality/src/app/components/question/listarquestion/listarquestion.component.ts
+++ Vitality/src/app/components/question/listarquestion/listarquestion.component.ts
@@ -1,26 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource, MatTableModule} from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { Question } from '../../../models/question';
 import { QuestionService } from '../../../services/question.service';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-listarquestion',
   standalone: true,
-  imports: [MatTableModule
-  ],
+  imports: [MatTableModule, MatIconModule, MatButtonModule,RouterLink],
   templateUrl: './listarquestion.component.html',
-  styleUrl: './listarquestion.component.css'
+  styleUrl: './listarquestion.component.css',
 })
-export class ListarquestionComponent implements OnInit{
-  displayedColumns:string[]=['idQuestion','consulta'];
+export class ListarquestionComponent implements OnInit {
+  displayedColumns: string[] = ['idQuestion', 'consulta','acciones'];
   dataSource: MatTableDataSource<Question> = new MatTableDataSource();
-  constructor(private qS:QuestionService) {}
-   ngOnInit(): void {
-    this.qS.list().subscribe((data)=>{
-      this.dataSource=new MatTableDataSource(data)
-    })
+  constructor(private qS: QuestionService) {}
+  ngOnInit(): void {
+    this.qS.list().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+    });
     this.qS.getList().subscribe((data) => {
       this.dataSource = new MatTableDataSource(data);
     });
-   }
+  }
+
+  eliminar(id: number) {
+    this.qS.eliminar(id).subscribe((data) => {
+      this.qS.list().subscribe((data) => {
+        this.qS.setList(data);
+      });
+    });
+  }
 }
