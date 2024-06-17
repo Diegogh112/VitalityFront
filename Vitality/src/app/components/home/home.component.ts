400d4d382b42a3118090461a1b4f296d80a86cb6
Diegogh112 <u202217814@upc.edu.pe>
2024-06-17 12:27:11 -0500

    a

--- Vitality/src/app/components/home/home.component.ts
+++ Vitality/src/app/components/home/home.component.ts
@@ -1,9 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
