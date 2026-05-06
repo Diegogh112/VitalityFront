2a9061e64a253ba2ae76f6d41caf779d493f87bc
Diegogh112 <u202217814@upc.edu.pe>
2026-05-06 15:34:36 -0500

    fix: resolve merge conflict in seguridad.guard.ts

--- Vitality/src/app/guard/seguridad.guard.ts
+++ Vitality/src/app/guard/seguridad.guard.ts
@@ -1,30 +1,14 @@
-<<<<<<< HEAD
-import { inject } from "@angular/core";
-import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from "@angular/router";
-import { LoginService } from "../services/login.service";
-
-export const segGuard = (
-  route: ActivatedRouteSnapshot,
-  state: RouterStateSnapshot
-) => {
-    const lService=inject(LoginService)
-=======
 import { inject } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
 import { LoginService } from '../services/login.service';
 
-export const segGuard = (route:ActivatedRouteSnapshot, state:RouterStateSnapshot) => {
-  const lService=inject(LoginService)
->>>>>>> fb4464bdd2d10e1debf9cfb6dba310edb07e5f23
-    const router=inject(Router)
-    const rpta=lService.verificar();
-    if(!rpta){
-      router.navigate(['/login']);
-      return false;
-    }
-    return rpta;
-<<<<<<< HEAD
+export const segGuard = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+  const lService = inject(LoginService);
+  const router = inject(Router);
+  const rpta = lService.verificar();
+  if (!rpta) {
+    router.navigate(['/login']);
+    return false;
+  }
+  return rpta;
 };
-=======
-};
->>>>>>> fb4464bdd2d10e1debf9cfb6dba310edb07e5f23
