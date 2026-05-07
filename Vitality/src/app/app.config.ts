519a7f77eb75ec0f6f5d132ca4e23b339f60e25a
Diegogh112 <u202217814@upc.edu.pe>
2026-05-07 12:46:08 -0500

    registrar compra

--- Vitality/src/app/app.config.ts
+++ Vitality/src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, importProvidersFrom} from '@angular/core';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
@@ -12,19 +12,17 @@ export function tokenGetter() {
   return sessionStorage.getItem('token');
 }
 
-const backendDomain = new URL(environment.base).host;
-
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
     provideAnimationsAsync(),
-    provideHttpClient(withFetch(),withInterceptorsFromDi()), 
+    provideHttpClient(withInterceptorsFromDi()), 
     importProvidersFrom(
       JwtModule.forRoot({
         config: {
           tokenGetter: tokenGetter,
-          allowedDomains: [backendDomain],
+          allowedDomains: ['vitality-arquitectura-web.onrender.com'],
           disallowedRoutes: [`${environment.base}/login/forget`],
         },
       })
